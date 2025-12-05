e78d2a8c9381de27177cedb7702a0c9155c3aa34
Vincent Agi <99982350+vincent-agi@users.noreply.github.com>
2025-12-05 23:35:27 +0100

    Create run.sh to automate Docker setup
    This script clones two repositories and starts their Docker containers.

---
+++ run.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+git clone https://github.com/vincent-agi/interoperabilite-SI.git
+cd interoperabilite-SI
+docker-compose up -d --build
+
+cd ..
+
+git clone https://github.com/stefanprodan/dockprom
+cd dockprom
+ADMIN_USER='admin' ADMIN_PASSWORD='admin' ADMIN_PASSWORD_HASH='$2a$14$1l.IozJx7xQRVmlkEQ32OeEEfP5mRxTpbDTCTcXRqn19gXD8YK1pO' docker-compose up -d --build
